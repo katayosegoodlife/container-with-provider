e32e1e9aa153b66f5c9eaa321d4255fae6b26efb
4kizuki <akizuki.c10.l65@gmail.com>
2017-11-03 08:13:18 +0900

    Add ProviderAbstract

---
+++ src/ProviderAbstract.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bellisq\ContainerWithProvider;
+
+use Bellisq\ContainerWithProvider\ObjectDefinitionRegister;
+
+
+/**
+ * [ Provider ] Abstract Provider
+ * 
+ * To deny multi-purpose classes, abstract class is provided instead of interface.
+ *
+ * @author 4kizuki <akizuki.c10.l65@gmail.com>
+ * @copyright 2017 4kizuki. All Rights Reserved.
+ * @package bellisq/container-with-provider
+ * @since 1.0.0
+ */
+abstract class ProviderAbstract
+{
+
+    /**
+     * [ Register ] Register what this class provides.
+     * 
+     * All providers must register what they provide in this method.
+     */
+    abstract public function register(ObjectDefinitionRegister $odr);
+
+    /**
+     * [ Factory ]
+     * 
+     * Singleton mechanism is already implemented in container so that
+     * instantiating is only required in this method.
+     */
+    abstract public function getInstance(string $name);
+    
+}
