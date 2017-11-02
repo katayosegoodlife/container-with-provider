86710396b61cc18d139bd535285bd4c2286aeb71
4kizuki <akizuki.c10.l65@gmail.com>
2017-11-03 07:50:55 +0900

    Add phpDoc for ObjectDefinitionTransport

--- src/Com/ObjectDefinitionTransport.php
+++ src/Com/ObjectDefinitionTransport.php
@@ -2,6 +2,15 @@
 
 namespace Bellisq\ContainerWithProvider\Com;
 
+
+/**
+ * [ Communication ] Data Transporter
+ *
+ * @author 4kizuki <akizuki.c10.l65@gmail.com>
+ * @copyright 2017 4kizuki. All Rights Reserved.
+ * @package bellisq/container-with-provider
+ * @since 1.0.0
+ */
 class ObjectDefinitionTransport
 {
 
