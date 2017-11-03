3c00cb65e69e1cb9790922ccfa37b0269e3e8b2e
4kizuki <akizuki.c10.l65@gmail.com>
2017-11-03 12:02:17 +0900

    Add phpDoc for InvalidBaseClassException

--- src/Exceptions/InvalidBaseClassException.php
+++ src/Exceptions/InvalidBaseClassException.php
@@ -5,6 +5,14 @@ namespace Bellisq\ContainerWithProvider\Exceptions;
 use DomainException;
 
 
+/**
+ * [ Exception ] Invalid Base Class
+ *
+ * @author 4kizuki <akizuki.c10.l65@gmail.com>
+ * @copyright 2017 4kizuki. All Rights Reserved.
+ * @package bellisq/container-with-provider
+ * @since 1.0.0
+ */
 class InvalidBaseClassException extends DomainException
 {
 
